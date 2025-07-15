d6befc89fae888a0317fb7eead349113dc63bd18
lfenderico-dev <lorenzofenderico.dev@gmail.com>
2025-07-15 17:14:43 +0200

    Added the GallerySection with some styling

---
+++ src/components/HomePage/GallerySection.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="gallerySection" class="flex flex-col">
+    <h1 class="text-center text-2xl text-white pt-5">Gallery</h1>
+    <div class="mx-5 h-full flex items-center">
+      <div class="flex overflow-x-auto w-full h-60">
+        <img src="/src/assets/photo/gallery-photos/image1.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image2.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image3.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image4.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image5.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image6.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image7.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image8.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image9.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image10.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image11.jpg" />
+        <img src="/src/assets/photo/gallery-photos/image12.jpg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+#gallerySection {
+  background-image: url('/src/assets/photo/blackBg.png');
+  height: 50vh;
+  width: 100vw;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,11 +3,13 @@
     <IntroSection />
     <AboutUs />
     <ServicesPage />
+    <GallerySection />
   </div>
 </template>
 
 <script setup lang="ts">
 import AboutUs from '@/components/HomePage/AboutUs.vue';
+import GallerySection from '@/components/HomePage/GallerySection.vue';
 import IntroSection from '@/components/HomePage/IntroSection.vue';
 import ServicesPage from '@/components/HomePage/ServicesPage.vue';
 
