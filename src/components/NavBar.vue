d3fb27b1a1b251dfce0f8bc1c945f1576f3d468f
lfenderico-dev <lorenzofenderico.dev@gmail.com>
2025-07-15 22:36:20 +0200

    Adjustments on the navbar size

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class=" bg-eerieBlack h-20 flex justify-start gap-10 px-3 py-1">
+  <header class=" bg-eerieBlack h-20 flex justify-start gap-5 px-3 py-1">
     <p id="logo" class="flex justify-start h-full items-center gap-1"><img class="w-8" src="/src/assets/icons/pizzaIcon.png" alt="Pizza image">PizzaTown</p>
     <nav class="text-white flex justify-end gap-3 h-full items-center">
       <router-link to="/">Home</router-link>
@@ -15,11 +15,11 @@
 
 <style scoped>
 header{
-  font-size: 15px;
+  font-size: 12px;
   color: white;
 }
 #logo {
-  font-size: 18px;
+  font-size: 16px;
 }
 
 
